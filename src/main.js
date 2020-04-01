0532abd9319f30fc9167565fc6c174dc7bd2406c
Daklin2 <daklin754@gmail.com>
2020-04-01 23:44:14 +0300

    feat: create keys

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import keys from './keys.js';
+
 class KeyBoard {
   constructor() {
     this.lang = 'ru';
     this.capsLock = false;
 
-    this.keyBoard = document.createElement('div');
+    this.keyboard = document.createElement('div');
     this.textArea = document.createElement('textarea');
     this.pageLangBtn = document.createElement('button');
   }
@@ -24,10 +26,26 @@ class KeyBoard {
     this.pageLangBtn.innerHTML = this.lang;
     wrapper.append(this.pageLangBtn);
 
-    this.keyBoard.classList.add('keyboard__container');
-    wrapper.append(this.keyBoard);
+    this.keyboard.classList.add('keyboard__container');
+    wrapper.append(this.keyboard);
+  }
+
+  createButtons() {
+    keys.forEach((keysRowIndex) => {
+      const keysRowContainer = document.createElement('div');
+      keysRowContainer.classList.add('row');
+      this.keyboard.append(keysRowContainer);
+
+      keysRowIndex.forEach((keyIndex) => {
+        const key = document.createElement('button');
+        if (keyIndex[0] !== '') key.classList.add('key', `${keyIndex[0]}`);
+        else key.classList.add('key');
+        keysRowContainer.append(key);
+      });
+    });
   }
 }
 
 const KEYBOARD = new KeyBoard();
 KEYBOARD.init();
+KEYBOARD.createButtons();
